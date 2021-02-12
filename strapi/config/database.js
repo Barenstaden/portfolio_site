964e2affd076947d3167aadc95456f4f675beec4
Thomas Bjørnstad <barenstaden@yahoo.no>
2021-02-12 17:20:50 +0100

    Db settings

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -4,15 +4,10 @@ module.exports = ({ env }) => ({
     default: {
       connector: "bookshelf",
       settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", ""),
-        password: env("DATABASE_PASSWORD", ""),
+        client: "sqlite",
       },
       options: {
-        ssl: false,
+        useNullAsDefault: true,
       },
     },
   },
