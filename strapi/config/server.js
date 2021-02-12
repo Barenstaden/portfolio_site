8e3034a9683bdd329458022941fa07bdbffc82fe
Thomas Bjørnstad <barenstaden@yahoo.no>
2021-02-12 16:46:08 +0100

    init

--- strapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '4763546e53dea0ed6fe7eaf2aefe96ac'),
-    },
-  },
-});
