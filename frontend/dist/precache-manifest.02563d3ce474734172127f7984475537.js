8667aee2e1d3da7f1c3b81008eb24a066f896704
Thomas Bjørnstad <barenstaden@yahoo.no>
2021-03-23 14:19:51 +0100

    Build

---
+++ frontend/dist/precache-manifest.02563d3ce474734172127f7984475537.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef56558c3faffa5ca8ad",
+    "url": "/css/about.08ca6ae6.css"
+  },
+  {
+    "revision": "7007ad5f0c305e04beb1",
+    "url": "/css/app.8bf528ca.css"
+  },
+  {
+    "revision": "28dd5b59ce7b689d7c61",
+    "url": "/css/chunk-24713e28.8a19fe07.css"
+  },
+  {
+    "revision": "d1aca9cc18b7c0832b62",
+    "url": "/css/chunk-74818558.af01bb81.css"
+  },
+  {
+    "revision": "14a3543a9981e7780ab5",
+    "url": "/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "42e9257837e5a83c87001994a6d662cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef56558c3faffa5ca8ad",
+    "url": "/js/about.04522815.js"
+  },
+  {
+    "revision": "7007ad5f0c305e04beb1",
+    "url": "/js/app.c2b5dcd6.js"
+  },
+  {
+    "revision": "28dd5b59ce7b689d7c61",
+    "url": "/js/chunk-24713e28.76887f15.js"
+  },
+  {
+    "revision": "8f1bada96b81e667dd5a",
+    "url": "/js/chunk-5d9ade4c.b20b502d.js"
+  },
+  {
+    "revision": "d1aca9cc18b7c0832b62",
+    "url": "/js/chunk-74818558.58711d06.js"
+  },
+  {
+    "revision": "14a3543a9981e7780ab5",
+    "url": "/js/chunk-vendors.e77c6b13.js"
+  },
+  {
+    "revision": "01f28ca1168495ae62b8ce9ed2349783",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
